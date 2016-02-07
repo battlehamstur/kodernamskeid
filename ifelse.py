92a8cc8c958cf0be8169471bf9c500df00fa2d2f
battlehamstur <gabriel.smarason@gmail.com>
2016-02-07 12:43:31 +0000

    ifelse.py
    Þetta er bara simple if og else coding

---
+++ ifelse.py
@@ -0,0 +1,10 @@
+from mcpi.minecraft import Minecraft
+
+mc = Minecraft.create()
+nyr_stadur = mc.getBlock(80, 4, 40)
+
+if nyr_stadur == 1:
+    mc.player.setPos(80, 20, 40)
+
+else:
+    print('Þessi blokk er úr hörðu efni og þú munt festast')
